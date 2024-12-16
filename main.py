cf66955fc63accc19687390ee976de84c8626135
Mojtaba <165335428+MojTabax2@users.noreply.github.com>
2024-12-16 18:44:09 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,12 @@ ads = ADS1115(i2c)
 fsr_channels = [AnalogIn(ads, i) for i in range(3)]  # 3 FSR sensors: Protein, Vegetables, Carbs
 
 # Firebase setup
-cred = credentials.Certificate('/home/pft2/Desktop/pyCode/apiKey/platefultdb-firebase-adminsdk-y4pj8-55a9d1aabe.json')
-firebase_admin.initialize_app(cred, {'databaseURL': 'https://platefultdb-default-rtdb.firebaseio.com/'})
-ref = db.reference('test/rpi/v2')
+cred = credentials.Certificate("/home/pft2/Desktop/pyCode/apiKey/plateful-2e88e-firebase-adminsdk-r247n-d58f64e0c0.json")
+firebase_admin.initialize_app(cred, {'databaseURL': 'https://plateful-2e88e-default-rtdb.firebaseio.com/Pilot/D0/'})
+PL = db.reference('Proteins')
+VL = db.reference('Veggie')
+CL = db.reference('Carbs')
+Report = db.reference('Report')
 
 # GPIO setup
 BUTTON_PIN = 17
@@ -52,7 +55,7 @@ def play_audio(file_path):
 
 # Calibration function
 def calibrate(channel):
-	return sum(channel.value for _ in range(50)) / 50  # Average over 10 readings
+	return sum(channel.value/100 for _ in range(10)) / 10  # Average over 10 readings
 
 
 
@@ -62,8 +65,8 @@ def tlr(section, method):
 		# Run the LED control script in the background using Popen
 		process = subprocess.Popen(
 			['sudo', '/home/pft2/myenv/bin/python3', '/home/pft2/Desktop/pyCode/pfss2.py', section, method],
-			stdout=subprocess.DEVNULL,  # Suppress output (you can redirect to a file if you want)
-			stderr=subprocess.DEVNULL,  # Suppress errors (you can redirect to a file if needed)
+			#stdout=subprocess.DEVNULL,  # Suppress output (you can redirect to a file if you want)
+			#stderr=subprocess.DEVNULL,  # Suppress errors (you can redirect to a file if needed)
 			preexec_fn=lambda: None  # Ensures the process doesn't inherit terminal settings
 		)
 		print("LED reaction triggered in the background.")
@@ -79,7 +82,7 @@ def tlr(section, method):
 def initiate_section(section):
 	tlr(section, 'light_up')
 	if section == 'Carbs':
-		play_audio(random.choice(audio_files[section]))
+		play_audio(random.choice(audio_files["carb"]))
 		print("carb audio played")
 
 	print(f"{section} section activated.")
@@ -103,129 +106,157 @@ section_new = {'Protein': False, 'Vegetables': False, 'Carbs': False}
 protein_activation_time = start_time + 180  # 3 minutes
 carbs_activation_time = start_time + 360  # 6 minutes
 
+pReact = 0
+vReact = 0
 
 
 # Main loop
 try:
-	# Initial greeting audio
-	play_audio(audio_files['greeting'])
-	#calibrated_values = [calibrate(ch) for ch in fsr_channels]
-
-
+	#tlr('Protein', 'light_off')
+	#tlr('Vegetables', 'light_off')
+	#tlr('Carbs', 'light_off')
 	while True:
-		current_time = time.time()
-		elapsed_time = current_time - start_time
-		last_checked = 0
-		
 		# Read the button state
 		button_state = GPIO.input(BUTTON_PIN)
-
-		# Read FSR sensors
-		fsr_values = [int(ch.value - cal) for ch, cal in zip(fsr_channels, calibrated_values)]
-		print(f"Protein={fsr_values[0]}, Vegetables={fsr_values[1]}, Carbs={fsr_values[2]}")
-
-		# Record data locally
-		if len(data_points_P) < 60 and current_time - last_db_saved >= 10:
-			data_points_P.append(fsr_values[0])
-			data_points_V.append(fsr_values[1])
-			data_points_C.append(fsr_values[2])
-			last_db_saved = current_time
-		
-		if current_time - last_db_update >= 15:
-			ref.set({'Protein': data_points_P, 'Vegetables': data_points_V, 'Carbs': data_points_C})
-			last_db_update = current_time
+		if button_state == GPIO.LOW:
+			# Initial greeting audio
+			play_audio(audio_files['greeting'])
 
 
+			while True:
+				current_time = time.time()
+				elapsed_time = current_time - start_time
+				last_checked = 0
+				
 
-		# Initiate sections at appropriate times
-		if not section_states['Vegetables'] and current_time >= start_time and current_time <= start_time + 60:
-			initiate_section('Vegetables')
-			section_states['Vegetables'] = True
-			section_new['Vegetables'] = True
+				# Read FSR sensors
+				fsr_values = [int(ch.value/100 - cal) for ch, cal in zip(fsr_channels, calibrated_values)]
+				print(f"Protein={fsr_values[0]}, Carbs={fsr_values[1]}, Vegetables={fsr_values[2]}")
 
-		if not section_states['Protein'] and current_time >= protein_activation_time and current_time <= start_time + 240:
-			initiate_section('Protein')
-			section_states['Protein'] = True
-			section_new['Protein'] = True
-
-		if not section_states['Carbs'] and current_time >= carbs_activation_time and current_time <= start_time + 420:
-			initiate_section('Carbs')
-			section_states['Carbs'] = True
-			section_new['Carbs'] = True
+				# Record data locally
+				if len(data_points_P) < 60 and current_time - last_db_saved >= 10:
+					data_points_P.append(fsr_values[0])
+					data_points_C.append(fsr_values[1])
+					data_points_V.append(fsr_values[2])
+					last_db_saved = current_time
+				
+				# DB receives data
+				if current_time - last_db_update >= 15:
+					PL.set(data_points_P[-1])
+					VL.set(data_points_V[-1])
+					CL.set(data_points_C[-1])
+					last_db_update = current_time
 
 
 
-		# Workflow logic for interaction
-		if section_states['Vegetables'] and section_new['Vegetables'] and not section_states['Protein'] and not section_states['Carbs'] and int(fsr_values[1]/100) != 0:
-			if fsr_values[1]/100 == data_points_V[-1]/100:  # Vegetables section not used
-				tlr('Vegetables', 'light_pulse')
-				if int(fsr_values[0]/100) != 0 and fsr_values[0]/100 < data_points_P[-1]/100:
-					play_audio(audio_files['vegetable'[2]])
-					print("protein instead of v")
-				elif int(fsr_values[2]/100) != 0 and fsr_values[2]/100 < data_points_C[-1]/100:
-					tlr('carbs','light_B')
-					play_audio(audio_files['vegetable'[2]])
-					print("carbs instead of V")
+				# Initiate sections at appropriate times
+				if not section_states['Vegetables'] and current_time >= start_time and current_time <= start_time + 60:
+					initiate_section('Vegetables')
+					section_states['Vegetables'] = True
+					section_new['Vegetables'] = True
 
-			elif fsr_values[1]/100 < data_points_V[-1]/100:			
-				play_audio(random.choice(audio_files['vegetable']))
-				play_audio(audio_files['Coin'])
-				print("1st time veggie")
-				section_new['Vegetables'] = False
+				if not section_states['Protein'] and current_time >= protein_activation_time and current_time <= start_time + 240:
+					initiate_section('Protein')
+					section_states['Protein'] = True
+					section_new['Protein'] = True
 
-		
-		if section_states['Protein'] and section_new['Protein'] and not section_states['Carbs'] and int(fsr_values[0]/100) != 0:
-			if fsr_values[0]/100 == data_points_P[-1]/100:  # protein section not used
-				tlr('Protein', 'light_pulse')
-				if int(fsr_values[2]/100) != 0 and fsr_values[2]/100 < data_points_C[-1]/100:
-					play_audio(audio_files['protein'[3]])
-					print("carbs instead of p")
+				if not section_states['Carbs'] and current_time >= carbs_activation_time and current_time <= start_time + 420:
+					initiate_section('Carbs')
+					section_states['Carbs'] = True
+					section_new['Carbs'] = True
+
+
+
+				# Workflow logic for interaction
+				if section_new['Vegetables'] and not section_states['Protein'] and not section_states['Carbs']:
+					if fsr_values[2] >= data_points_V[-1]:  # Vegetables section not used
+						tlr('Vegetables', 'light_pulse')
+						if fsr_values[0] < data_points_P[-1]:
+							play_audio(audio_files['vegetable'][2])
+							print("protein instead of v")
+						elif fsr_values[1] < data_points_C[-1]:
+							tlr('carbs','light_B')
+							play_audio(audio_files['vegetable'][2])
+							print("carbs instead of V")
+
+					elif fsr_values[2] < data_points_V[-1]:			
+						play_audio(random.choice(audio_files['vegetable']))
+						play_audio(audio_files['Coin'])
+						print("1st time veggie")
+						section_new['Vegetables'] = False
+
+				
+				if section_new['Protein'] and not section_states['Carbs']:
+					if fsr_values[0] >= data_points_P[-1]:  # protein section not used
+						tlr('Protein', 'light_pulse')
+						if int(fsr_values[1]/100) != 0 and fsr_values[1]/100 < data_points_C[-1]/100:
+							play_audio(audio_files['protein'][3])
+							print("carbs instead of p")
+							
+					elif fsr_values[0] < data_points_P[-1]:				
+						play_audio(random.choice(audio_files['protein']))
+						play_audio(audio_files['Coin'])
+						print("first bite pro")
+						section_new['Protein'] = False
+
+
+
+				#activity
+				if (elapsed_time - last_checked) > 180:
+					last_checked = elapsed_time
+					if fsr_values[0] >= (sum(data_points_P) / len(data_points_P))*0.9:
+						play_audio(audio_files['protein'][3])
+						tlr('Protein', 'light_pulse')
+						print("protein untouched")
+					elif pReact <= 3:
+						play_audio(random.choice(audio_files['protein']))
+						pReact += 1
+						play_audio(audio_files['Coin'])
 					
-			elif fsr_values[0]/100 < data_points_P[-1]/100:				
-				play_audio(random.choice(audio_files['protein']))
-				play_audio(audio_files['Coin'])
-				print("first bite p")
-				section_new['Protein'] = False
+					if fsr_values[2] >= (sum(data_points_V) / len(data_points_V))*0.9:
+						play_audio(audio_files['vegetable'][2])
+						tlr('Vegetables', 'light_pulse')
+						print("veggie untouched")
+					elif vReact <= 3:
+						play_audio(random.choice(audio_files['vegetable']))
+						vReact +=1
+						play_audio(audio_files['Coin'])
+				
 
 
 
-		#activity
-		if elapsed_time - last_checked > 120:
-			last_checked = elapsed_time
-			if fsr_values[0]/100 > (sum(data_points_P) / len(data_points_P))*0.9:
-				play_audio(audio_files['protein'[3]])
-				print("protein untouched")
-			else:
-				play_audio(random.choice(audio_files['protein']))
-				play_audio(audio_files['Coin'])
-			
-			if fsr_values[1]/100 > (sum(data_points_V) / len(data_points_V))*0.9:
-				play_audio(audio_files['vegetable'[2]])
-				print("veggie untouched")
-			else:
-				play_audio(random.choice(audio_files['vegetable']))
-				play_audio(audio_files['Coin'])
-		
+				# light off logic
+				if elapsed_time > 360:
+					if fsr_values[0] < (data_points_P[1])*0.4:  # Turn off light when FSR drops below 40%
+						tlr('Protein', 'light_off')
+						section_states['Protein'] = False
+					if fsr_values[2] < (data_points_V[1])*0.4:  # Turn off light when FSR drops below 40%
+						tlr('Vegetables', 'light_off')
+						section_states['Vegetables'] = False
+					if fsr_values[1] < (data_points_C[1])*0.4:  # Turn off light when FSR drops below 40%
+						tlr('Carbs', 'light_off')
+						section_states['Carbs'] = False
+
+					if section_states['Carbs'] == False and section_states['Vegetables'] == False and section_states['Protein'] == False:
+						break
 
 
 
-		# light off logic
-		if elapsed_time > 300:
-			if fsr_values[0]/100 < (data_points_P[-1]/100)*0.4:  # Turn off light when FSR drops below 40%
-				tlr('Protein', 'light_off')
-			if fsr_values[1]/100 < (data_points_V[-1]/100)*0.4:  # Turn off light when FSR drops below 40%
-				tlr('Vegetables', 'light_off')
-			if fsr_values[2]/100 < (data_points_C[-1]/100)*0.4:  # Turn off light when FSR drops below 40%
-				tlr('Carbs', 'light_off')
-
-
-
-		time.sleep(1)  # Wait 5 seconds before next reading
+				time.sleep(1)  # Wait 5 seconds before next reading
+				
+			break
 
 except KeyboardInterrupt:
 	print("Script interrupted. Cleaning up GPIO...")
 
 finally:
+	rep = {
+		"Protein": data_points_P,
+    	"Carbs": data_points_C,
+    	"Veggie": data_points_V
+	}
+	Report.set(rep)
+
 	tlr('Protein', 'light_off')
 	tlr('Vegetables', 'light_off')
 	tlr('Carbs', 'light_off')
